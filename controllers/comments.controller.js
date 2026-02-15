c5f8daf3fbe6f4a0d98db078fe1635be75ff4508
bhanupsh <chbprakash.js@gmail.com>
2026-02-15 23:18:48 +0530

    added limit

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -3,13 +3,14 @@ import express from "express";
 import Comment from "../models/comments.model.js";
 
 export const getAllComments = async (req, res) => {
-    const comments = await Comment.find();  
-    // Logic to get all comments
+    const comments = await Comment.find().limit(20);  
+    // Logic to get all comments20
     res.status(200).json({ message: "Comments retrieved successfully....123", comments });
 };
 
 export const getCommentById = async (req, res) => {
     const { id } = req.params;
+    const comments = await Comment.findById(id);
     // Logic to get a specific comment by ID
-    res.status(200).json({ message: `Comment with ID ${id} retrieved successfully....678` });
+    res.status(200).json({ message: `Comment with ID ${id} retrieved successfully....678`, comments });
 };
